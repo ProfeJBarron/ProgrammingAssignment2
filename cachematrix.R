703a87cf7f09ce3f833cbf116ec3b556800817e8
ProfeJBarron <jesus.barvid@yahoo.com.mx>
2015-08-03 19:51:44 -0500

    Set original file to "cachematrix_old.R" and write the code necessary for the assessment into the "cachematrix.R". I tested functions and seems to work fine.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Here we have a pair of functions that are designed to compute the inverse
+## of a matrix in a cached way to optimize the time.
 
-## Write a short comment describing this function
+## This functions creates a "special matrix" to be able to cached the inverse
+## of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+    inv <- NULL
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set, get = get,
+        setInverse = setInverse,
+        getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## This function receives a "special matrix" and search for the cached inverse
+## if not exists then is computed and cached
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+    i <- x$getInverse()
+    if(!is.null(i)) {
+      message("getting cached inverse matrix")
+      return(i)
+    }
+    m <- x$get()
+    i <- solve(m)
+    x$setInverse(i)
+    i
+
 }
+
+## To test functions do:
+## 1-create a square matrix, say "mm" (be sure it has inverse)
+## 2- execute ">sm <- makeCacheMatrix(mm)" to get the special matrix
+## 3-execute ">cacheSolve(sm)" to compute the inverse
+## 4-execute again ">cacheSolve(sm)" to get the cached inverse
+
+
---
+++ cachematrix_old.R
@@ -0,0 +1,17 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+# function to create a special matrix to be cached
+
+makeCacheMatrix <- function(x = matrix()) {
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
